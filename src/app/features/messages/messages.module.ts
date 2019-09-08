ca37b974336ea4cbb40f2cdffb77fbf594ef67c3
Shadab Faiz <shadab.faiz@dhwaniris.com>
2019-09-08 16:30:20 +0530

    Added chat functionality without any user

--- src/app/features/messages/messages.module.ts
+++ src/app/features/messages/messages.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -20,6 +21,8 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     MatTabsModule,
     MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule
   ]
 })
--- src/app/features/messages/messages/messages.component.ts
+++ src/app/features/messages/messages/messages.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { chatOption } from 'src/app/models/chatOptions';
 
 @Component({
@@ -7,13 +8,68 @@ import { chatOption } from 'src/app/models/chatOptions';
   styleUrls: ["./messages.component.scss"]
 })
 export class MessagesComponent implements OnInit {
+  @ViewChild("chatBox", { static: true }) chatBox: ElementRef<any>;
   chatOption = chatOption;
+  chatForm: FormGroup;
 
-  constructor() {}
+  chatHistory = [
+    {
+      user: "you",
+      message: "Hellooooo there... General Kanobi",
+      date: Date.now()
+    },
+    {
+      user: "you",
+      message: "Hellooooo there... General Kanobi",
+      date: Date.now()
+    },
+    {
+      user: "you",
+      message: "Hellooooo there... General Kanobi",
+      date: Date.now()
+    }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.createChatForms();
+  }
 
   ngOnInit() {}
 
   togleOption(element: Element) {
     element.classList.toggle(`show`);
+  }
+
+  onUserMessageSubmit(form: FormGroup) {
+    this.addMessageToChatHistory(form);
+    form.reset();
+    this.scrollChatBoxToBottom();
+  }
+
+  addMessageToChatHistory(form: FormGroup) {
+    const message = form.controls.message.value;
+    this.chatHistory = [...this.chatHistory, this.createMesssageObj(message)];
+  }
+
+  private createChatForms() {
+    this.chatForm = this.fb.group({
+      message: ""
+    });
+  }
+
+  private createMesssageObj(message: string) {
+    return {
+      message,
+      user: "you",
+      date: Date.now()
+    };
+  }
+
+  private scrollChatBoxToBottom() {
+    setTimeout(() => {
+      this.chatBox.nativeElement.scrollTop =
+        this.chatBox.nativeElement.scrollHeight + 100;
+      console.log(this.chatBox.nativeElement.scrollHeight);
+    }, 50);
   }
 }
